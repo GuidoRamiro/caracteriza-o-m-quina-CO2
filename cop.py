1a42534e8d8bd4d7e4273e435aab54ed2d5f162c
Guilherme Ramiro <127567752+GuidoRamiro@users.noreply.github.com>
2024-08-19 19:58:12 -0300

    Update cop.py

--- cop.py
+++ cop.py
@@ -7,6 +7,7 @@ dados_vazao = pd.read_excel(r"C:\Users\guilh\OneDrive\UFMG\IC\caracterização m
 dados_temperatura = pd.read_excel(r"C:\Users\guilh\OneDrive\UFMG\IC\caracterização máquina co2\testes\17°C\86 bar\17_86_temperatura.xlsx")
 
 tempo_lista = []
+pot_frig_lista = []
 cop_lista = []
 
 cp = CP.PropsSI('C', 'T', 298.15, 'P', 101325.0, 'water')
@@ -16,14 +17,16 @@ compressor_pot = 600
 for index, row in dados_temperatura.iterrows():
     t21 = row['T21'] + 273.15  
     t22 = row['T22'] + 273.15  
-    m_h20 = dados_vazao.at[index, 'V2']/60  
-
-    cop = abs((cp * m_h20 * (t21 - t22)) / compressor_pot)  
-   
+    m_h2o = dados_vazao.at[index, 'V2']/60  
+    pot_frig = abs(cp* m_h2o * (t21-t22))
+    cop = pot_frig / compressor_pot
+    
+    pot_frig_lista.append(pot_frig)
     cop_lista.append(cop)
     tempo_lista.append(row['tempo, s'])  
 
 cop_media = np.mean(cop_lista)
+pot_frig_media = np.mean(pot_frig_lista)
 
 plt.figure(figsize=(10, 6))
 plt.plot(tempo_lista, cop_lista, label='COP')
@@ -34,6 +37,6 @@ plt.legend()
 plt.grid(True)
 plt.show()
 
-df_cop = pd.DataFrame({'tempo, s': tempo_lista, 'COP': cop_lista, 'COP Médio': [cop_media] * len(tempo_lista)})
+df_cop = pd.DataFrame({'tempo, s': tempo_lista, 'Potência frigorífica, W': pot_frig_media, 'COP': cop_lista, 'COP Médio': [cop_media] * len(tempo_lista)})
 
 df_cop.to_excel(r"C:\Users\guilh\OneDrive\UFMG\IC\caracterização máquina co2\testes\17°C\86 bar\COP_17_86.xlsx", index=False)
